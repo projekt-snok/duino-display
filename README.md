fb70fa1db8642db0c3a3b98ff497833b689c9424
kmpm <peter@birchroad.net>
2017-07-30 23:01:14 +0200

    bilder och bättre kod

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 duino-display
 =============
-shwpn
+Code to test the display on a Biltema LMR24 robot mower.
+
+It seems as if it's a Hitachi HD44780 compatible display. It looked like it
+and when I tried the LiquidCrystal library from arduino it all worked.
 
 # Links
 * https://www.arduino.cc/en/Tutorial/LiquidCrystalDisplay
--- sketchbooks/duino-display/duino-display.ino
+++ sketchbooks/duino-display/duino-display.ino
@@ -1,42 +1,79 @@
 #include <LiquidCrystal.h>
 
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  duino-display
+  Test the display from a Biltema LMR24 Robot Mower.
+
+  Toggles backlight 10 seconds on and 2 seconds off.
+  Toggles display of text every second
+
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// Pin 11 has the LED on Teensy 2.0
-// Pin 6  has the LED on Teensy++ 2.0
-// Pin 13 has the LED on Teensy 3.0
-// give it a name:
-int led = 13;
+ #define DELAY_BACKLIGHT_LONG 10000
+ #define DELAY_BACKLIGHT_SHORT 2000
 
 const int rs = 12, en = 9, rw=10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 const int bl=11;
+
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+unsigned long now_millis;
+unsigned long next_display_millis;
+unsigned long next_backlight_millis;
+
+bool display_state = false;
+bool backlight_state = false;
 
 
 // the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(rw, OUTPUT);
-  pinMode(led, OUTPUT);
-  pinMode(bl, OUTPUT);
+void setup() { 
+  now_millis = millis();
+  next_display_millis = now_millis + 500;
+  next_backlight_millis = now_millis + DELAY_BACKLIGHT_LONG;
+  // initialize the digital pins as an output.
+  pinMode(rw, OUTPUT); //low to send data
+  pinMode(bl, OUTPUT); //backlight on/off
+  
+  // 16x2 character display
+  lcd.begin(16, 2); 
+
+  //begin write
   digitalWrite(rw, LOW);
-  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
   lcd.print("Biltema lmr24");
+  lcd.setCursor(0, 1);
+  lcd.print("Projekt SNOK");
+
+  //turn on backlight
   digitalWrite(bl, HIGH);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  lcd.display();
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  lcd.noDisplay();
-  delay(1000);               // wait for a second
+  now_millis = millis();
+  
+  if (now_millis > next_display_millis) {
+    display_state = !display_state;
+    if (display_state) {
+      lcd.display();
+    }
+    else {
+      lcd.noDisplay();
+    }
+    //next shift of display state in 1.5 seconds
+    next_display_millis = now_millis + 1000;
+  }
+
+  if (now_millis > next_backlight_millis) {
+    backlight_state = !backlight_state;
+    digitalWrite(bl, backlight_state);
+
+    if (backlight_state) {
+      next_backlight_millis = now_millis + DELAY_BACKLIGHT_LONG;
+    }
+    else {
+      next_backlight_millis = now_millis + DELAY_BACKLIGHT_SHORT;
+    }
+  }
+
 }
